c0f87e40bb18af031926886315b7d9362d997329
Slasten Tikhon <slasten32@gmail.com>
2018-12-17 23:30:18 +1000

    Add encrypt RSA

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from rsa import Rsa
 from utils import read_int
 
 def rsa():
-    image_stream = open('tests/output.jpg', 'wb')
+    """
+    Test RSA
+    """
 
-    with open("tests/encrypted.txt", "rb") as encrypted:
+    # Decrypt
+    res_image_stream = open('decrypt_rsa.jpg', 'wb')
+    with open('tests/encrypted.txt', 'rb') as encrypted:
         d = read_int(encrypted)
         n = read_int(encrypted)
 
@@ -18,11 +22,35 @@ def rsa():
         e = 8486391357485896322440256533131770617523773615720428244581337759609685018543
 
         rsa = Rsa(n, e, d)
-        rsa.decrypt(encrypted, image_stream)
+        rsa.decrypt(encrypted, res_image_stream)
 
-    image_stream.close()
+    res_image_stream.close()
 
+    # Encrypt
+    res_encrypted = open('encrypted_rsa.txt', 'wb')
+    with open('tests/decrypted.jpg', 'rb') as image_stream:
+        rsa.encrypt(image_stream, res_encrypted)
 
+    res_encrypted.close()
+
+    # Test encrypt
+    test_encrypt = open('test_encrypt.jpg', 'wb')
+    with open('encrypted_rsa.txt', 'rb') as res_encrypted:
+        d = read_int(res_encrypted)
+        n = read_int(res_encrypted)
+
+        if d != 6126098115646990325497939754751346680273131840506753785808675623744493660811:
+            raise Exception('Not equal d')
+
+        if n != 11714199531846391552190233593088023207845451372578311171850728357335472002497:
+            raise Exception('Not equal n')
+
+        e = 8486391357485896322440256533131770617523773615720428244581337759609685018543
+
+        rsa = Rsa(n, e, d)
+        rsa.decrypt(res_encrypted, test_encrypt)
+
+    test_encrypt.close()
 
 if __name__ == "__main__":
     rsa()
--- rsa.py
+++ rsa.py
@@ -8,10 +8,10 @@ class Rsa:
         self.d = d
     
     def decrypt(self, reader, writer):
-        len = os.fstat(reader.fileno()).st_size
+        length = os.fstat(reader.fileno()).st_size
         pos = reader.tell()
 
-        while pos < len - 32:
+        while pos < length - 32:
             c = pow(read_int(reader), self.d, self.n)
             bytes_arr = c.to_bytes(8, byteorder='big')
             writer.write(bytes_arr)
@@ -23,4 +23,22 @@ class Rsa:
         if bytes_arr[7] != 0x08:
             writer.write(bytes_arr[:8 - bytes_arr[7]])
 
-            
+    def encrypt(self, reader, writer):
+        writer.write(self.d.to_bytes(32, byteorder='big'))
+        writer.write(self.n.to_bytes(32, byteorder='big'))
+
+        length = os.fstat(reader.fileno()).st_size
+        pos = reader.tell()
+
+        while pos < length:
+            bytes_arr = reader.read(8)
+
+            if len(bytes_arr) < 8:
+                l = 8 - len(bytes_arr)
+                bytes_arr += l.to_bytes(1, byteorder='big') * l
+
+            writer.write(pow(int.from_bytes(bytes_arr, byteorder='big'), self.e, self.n).to_bytes(32, byteorder='big'))
+            pos += 8
+
+        if length % 8 == 0:
+            writer.write(pow(int.from_bytes([0x08] * 8, byteorder='big'), self.e, self.n).to_bytes(32, byteorder='big'))            
